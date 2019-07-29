f6f44c8b1fb68e80be0489bd939ad9252e35236a
willyowi <wilsonowino1@gmail.com>
2019-07-29 10:03:38 +0300

    modified config

--- app/main/views.py
+++ app/main/views.py
@@ -5,17 +5,17 @@ from ..request import get_sources,get_article
 from . import main
 
 
-@main.route('/')
-def index():
-    """
-    Function that returns the index page and all its data
-    """
-    entertainment_news = get_sources('entertainment')
-    sport_news = get_sources('sports')
-    tech_news = get_sources('technology')
-    title = 'News Hightlight'
-    return render_template('index.html', title=title, sports=sport_news, technology=tech_news, entertainment=entertainment_news)
-
+# @main.route('/')
+# def index():
+#     """
+#     Function that returns the index page and all its data
+#     """
+#     entertainment_news = get_sources('entertainment')
+#     sport_news = get_sources('sports')
+#     tech_news = get_sources('technology')
+#     title = 'News Hightlight'
+#     return render_template('index.html', title=title, sports=sport_news, technology=tech_news, entertainment=entertainment_news)
+#
 @main.route('/source/<id>')
 def articles(id):
     """
