4a5f66e643cbe6191925ce87f4559a04d249955c
willyowi <wilsonowino1@gmail.com>
2019-07-29 10:06:47 +0300

    classes on models

--- app/models.py
+++ app/models.py
@@ -1,16 +1,16 @@
-class Sources:
-    """
-    Sources class to define News Objects
-    """
-
-    def __init__(self, id, name, description, url, category, country, language):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.url = url
-        self.category = category
-        self.country = country
-        self.language = language
+# class Sources:
+#     """
+#     Sources class to define News Objects
+#     """
+#
+#     def __init__(self, id, name, description, url, category, country, language):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.url = url
+#         self.category = category
+#         self.country = country
+#         self.language = language
 
 class Article:
     """
